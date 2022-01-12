bdcc8e2690d486ce8208f656d9828680d4944675
Ismail Bourbie <bourbieismail@gmail.com>
2022-01-12 14:14:40 +0100

    added Laboratory routes

--- app/Http/Controllers/Api/LaboratoryController.php
+++ app/Http/Controllers/Api/LaboratoryController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Laboratory;
 use Illuminate\Http\Request;
 
 class LaboratoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Laboratory::all());
+    }
+
+    public function show(Laboratory $laboratory)
+    {
+        return response()->json($laboratory);
+    }
 }
