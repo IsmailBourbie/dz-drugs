edbe28fb012b540517d54421cebf4c1959e7d07a
Ismail Bourbie <bourbieismail@gmail.com>
2022-01-12 14:07:57 +0100

    added Dci routes, handle ModelNotFoundExcption

--- app/Http/Controllers/Api/DciController.php
+++ app/Http/Controllers/Api/DciController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dci;
 use Illuminate\Http\Request;
 
 class DciController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Dci::all());
+    }
+
+    public function show(Dci $dci)
+    {
+        return response()->json($dci);
+    }
 }
