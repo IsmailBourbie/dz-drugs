187c6e74e742ec0d30ff74c4b0d4d4dc4afe5d2e
Ismail Bourbie <bourbieismail@gmail.com>
2022-02-07 14:37:17 +0100

    check if the query is valid

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -12,18 +12,24 @@ class SearchController extends Controller
     public function index(Request $request)
     {
         $q = $request->input('q');
-        $limit = $request->input('limit') ?? '*';
+        $limit = $request->input('limit', '*');
+
+        // if (strlen($q) < 3)
+        //     return response()->json([
+        //         'message' => 'Search query length must be grater than 3 charcters'
+        //     ], 400);
+
 
         // Search in drug name
         $drugs = Drug::query()->where('name', 'LIKE', "%{$q}%")->limit($limit)->get();
 
         // Search in Dci name
         $dci = Dci::query()->where('name', 'LIKE', "%{$q}%")->limit($limit)->get();
-        
+
 
         return response()->json([
             'drugs' => $drugs,
-            'dci' => $dci 
+            'dci' => $dci
         ], 200);
     }
 }
