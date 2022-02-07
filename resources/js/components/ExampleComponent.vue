b491533e7e172ee77406caea9ab3183dd07c9059
Ismail Bourbie <bourbieismail@gmail.com>
2022-02-07 12:22:01 +0100

    added vue@3 and tailwind

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container text-center">
+    <h1 class="text-3xl font-bold">Hello world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  }
+}
+</script>
