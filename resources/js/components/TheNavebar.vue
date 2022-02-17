b1470b41f63bd5009aacd8aab76ceaa757c75a25
Ismail Bourbie <bourbieismail@gmail.com>
2022-02-17 12:13:34 +0100

    added dark-mode switcher

--- resources/js/components/TheNavebar.vue
+++ resources/js/components/TheNavebar.vue
@@ -11,15 +11,13 @@
                 items-center
             "
         >
-            <a class="md:mb-0 ml-3 text-2xl font-black"> DRUDZ </a>
+            <a href="#" class="md:mb-0 ml-3 text-2xl font-black"> DRUDZ </a>
             <search-input-group class="w-80" v-if="currentRouteName !== 'Home'">
                 <search-input class="p-2" placeholder="Search drugs..." />
             </search-input-group>
             <nav class="flex flex-wrap items-center text-base justify-center">
-                <a class="mr-5 cursor-pointer">
-                    <svg-icon src="/images/icons/toggle-mode-btn.svg" />
-                </a>
-                <a class="mr-5 cursor-pointer">
+                <theme-switcher :theme="theme" @themeChanged="updateTheme" />
+                <a href="#" class="mr-5 cursor-pointer">
                     <svg-icon src="/images/icons/menu-btn.svg" />
                 </a>
             </nav>
@@ -30,17 +28,40 @@
 <script>
 import SearchInput from "../components/SearchInput.vue";
 import SearchInputGroup from "../components/SearchInputGroup.vue";
+import ThemeSwitcher from "../components/ThemeSwitcher.vue"
 export default {
-    components: { SearchInput, SearchInputGroup },
+    components: { SearchInput, SearchInputGroup, ThemeSwitcher },
     props: {
         searchInputVisible: {
             default: true
         }
     },
-     computed : {
-        currentRouteName () {
+    data() {
+        return {
+            theme: ''
+        }
+    },
+    created() {
+        this.setupCurrentTheme()
+    },
+    mounted() {
+        this.setupCurrentTheme()
+    },
+    methods: {
+        updateTheme(theme) {
+            this.theme = theme
+            document.querySelector('html').classList.remove('dark')
+            document.querySelector('html').classList.add(theme)
+        },
+        setupCurrentTheme() {
+            const currentTheme = localStorage.getItem('theme') || 'light';
+            document.querySelector('html').classList.add(currentTheme)
+        }
+    },
+    computed: {
+        currentRouteName() {
             return this.$route.name
         }
-    }
+    },
 };
 </script>
---
+++ resources/js/components/ThemeSwitcher.vue
@@ -0,0 +1,26 @@
+<template>
+    <a href="#" class="mr-5 cursor-pointer" @click.prevent="toggleTheme">
+        <svg-icon src="/images/icons/toggle-mode-btn.svg" />
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        theme: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        toggleTheme() {
+            const newTheme = this.theme === 'light' ? 'dark' : 'light';
+            localStorage.setItem('theme', newTheme);
+            this.$emit('themeChanged', newTheme);
+        },
+    },
+}
+</script>
+
+<style>
+</style>
